aac6b1e2a4a6db2676793e5b15d89b1142124db4
卜俊豪 <15755337832@163.com>
2020-07-03 16:25:30 +0800

    添加项目文件。

---
+++ GoogleTranslation/Controllers/MainController.cs
@@ -0,0 +1,43 @@
+﻿using GoogleTranslate.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace GoogleTranslation.Controllers
+{
+    public class MainController : Controller
+    {
+        // GET: Main
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public JsonResult GetTranslete(string chinese)
+        {
+            Translate translate = new Translate();
+            var translateResult = translate.GoogleTranslate(chinese, "zh-CN", "en");
+            var wordList = System.Text.RegularExpressions.Regex.Split(translateResult, @"\s+");
+            var str = "";
+            foreach (var item in wordList)
+            {
+                str += item.Substring(0, 1).ToUpper() + item.Substring(1);
+            }
+            var result = new Result()
+            {
+                Original = translateResult,
+                Formate = str
+            };
+            return Json(result);
+        }
+
+        public class Result
+        {
+            public string Original { get; set; }
+
+            public string Formate { get; set; }
+        }
+    }
+}
---
+++ GoogleTranslation/Global.asax.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace GoogleTranslation
+{
+    public class MvcApplication : System.Web.HttpApplication
+    {
+        protected void Application_Start()
+        {
+            AreaRegistration.RegisterAllAreas();
+            RegisterRoutes(RouteTable.Routes);
+        }
+
+        public static void RegisterRoutes(RouteCollection routes)
+        {
+            routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+
+            routes.MapRoute(
+                "Default", // 路由名称
+                "{controller}/{action}/{id}", // 带有参数的 URL
+                new { controller = "Main", action = "Index", id = UrlParameter.Optional } // 参数默认值
+            );
+        }
+    }
+}
